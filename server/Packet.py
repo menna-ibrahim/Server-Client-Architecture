0ee20c64808754536e0499ee7c8cade3834c3b90
Abdelrahman Salem <88258538+AbdelrahmanSalem9@users.noreply.github.com>
2022-05-15 21:17:11 +0200

    Update Packet.py

--- server/Packet.py
+++ server/Packet.py
@@ -2,7 +2,6 @@ class Packet:
     def __init__(self, msg, timestamp):
         self.msg = msg
         self.timestamp = timestamp
-        self.thread_id = thread_id
 
     def __lt__(self, other):
         return self.timestamp < other.timestamp
